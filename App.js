7d7a04bf93f5e649267691cf6d25be528c203dc5
Inderbir Singh <indsingh11@WKMZTC5D52AB.global.publicisgroupe.net>
2023-06-28 16:50:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+
+
+
+/*
+<div id="parent">
+<div id="child">
+<h1 id="heading1">head 1</h1>
+<h2 id=" heading2">head2</h1>
+</div>
+
+
+*/
+const parent =React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
+[ React.createElement("h1",{id:"heading1"},"this head one"),React.createElement("h2",{id:"heading2"},"heading two")   ]))
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
